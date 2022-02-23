45618b29c97e0b96b5d7188f4a3025e731f6aa46
Oleksandr Riabokin <alex1rap.wen.ru@gmail.com>
2022-02-23 16:10:37 +0200

    Add tests for vehicles updating

--- tests/Controller/VehicleControllerTest.php
+++ tests/Controller/VehicleControllerTest.php
@@ -3,15 +3,21 @@
 namespace App\Tests\Controller;
 
 use App\Service\Vehicle\VehicleService;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class VehicleControllerTest extends WebTestCase
 {
 
+    /**
+     * @var KernelBrowser|null
+     */
+    protected $client = null;
+
     public function testCarList(): void
     {
-        $client = static::createClient();
-        $client->request(
+        $this->client = $this->client ?? static::createClient();
+        $this->client->request(
             'POST',
             '/api/vehicles',
             [],
@@ -21,7 +27,7 @@ class VehicleControllerTest extends WebTestCase
                 'type' => VehicleService::TYPE_CAR
             ])
         );
-        $response = $client->getResponse();
+        $response = $this->client->getResponse();
         $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
@@ -29,10 +35,14 @@ class VehicleControllerTest extends WebTestCase
         $this->assertArrayHasKey('items', $responseData);
     }
 
-    protected function testVehicleCreation(array $vehicleData): void
+    /**
+     * @param array $vehicleData
+     * @return array
+     */
+    protected function testVehicleCreation(array $vehicleData): array
     {
-        $client = static::createClient();
-        $client->request(
+        $this->client = $this->client ?? static::createClient();
+        $this->client->request(
             'POST',
             '/api/vehicle',
             [],
@@ -40,7 +50,7 @@ class VehicleControllerTest extends WebTestCase
             ['CONTENT_TYPE' => 'application/json'],
             json_encode($vehicleData)
         );
-        $response = $client->getResponse();
+        $response = $this->client->getResponse();
         $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
@@ -50,11 +60,12 @@ class VehicleControllerTest extends WebTestCase
         foreach ($vehicleData as $key => $value) {
             $this->assertEquals($value, $responseData['vehicle'][$key]);
         }
+        return $responseData;
     }
 
     public function testCarCreation(): void
     {
-        $this->testVehicleCreation([
+        $carData = $this->testVehicleCreation([
             'model' => 'T13110 Sens',
             'brand' => 'Daewoo',
             'status' => VehicleService::STATUS_AVAILABLE,
@@ -66,7 +77,7 @@ class VehicleControllerTest extends WebTestCase
 
     public function testTruckCreation(): void
     {
-        $this->testVehicleCreation([
+        $truckData = $this->testVehicleCreation([
             "model" => "R420",
             "brand" => "Scania",
             "type" => "truck",
@@ -74,6 +85,64 @@ class VehicleControllerTest extends WebTestCase
             "price" => 6000000,
             "pollution_certificate" => "A"
         ]);
+    }
+
+    /**
+     * @param array $vehicleData
+     * @param int $id
+     * @return array
+     */
+    protected function testVehicleUpdating(array $vehicleData, int $id): array
+    {
+        $this->client = $this->client ?? static::createClient();
+        $this->client->request(
+            'PATCH',
+            '/api/vehicle/' . $id,
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($vehicleData)
+        );
+        $response = $this->client->getResponse();
+        $this->assertTrue($response->headers->contains('Content-Type', 'application/json'));
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('status', $responseData);
+        $this->assertArrayHasKey('vehicle', $responseData);
+        $this->assertEquals('updated', $responseData['status']);
+        foreach ($vehicleData as $key => $value) {
+            $this->assertEquals($value, $responseData['vehicle'][$key]);
+        }
+        return $responseData;
+    }
+
+    public function testCarCreationAndUpdating(): void
+    {
+        $carData = $this->testVehicleCreation([
+            'model' => 'T13110 Sens',
+            'brand' => 'Daewoo',
+            'status' => VehicleService::STATUS_AVAILABLE,
+            'price' => 65000,
+            'type' => VehicleService::TYPE_CAR,
+            'seats' => 5
+        ])['vehicle'];
+        $carData['price'] = 60000;
+        $this->testVehicleUpdating($carData, $carData['id']);
+    }
+
+    public function testTruckCreationAndUpdating(): void
+    {
+        $truckData = $this->testVehicleCreation([
+            "model" => "R420",
+            "brand" => "Scania",
+            "type" => "truck",
+            "status" => "sold",
+            "price" => 6000000,
+            "pollution_certificate" => "A"
+        ])['vehicle'];
+        $truckData['price'] = 5750000;
+        $truckData['pollution_certificate'] = 'B';
+        $this->testVehicleUpdating($truckData, $truckData['id']);
     }
 
 }
